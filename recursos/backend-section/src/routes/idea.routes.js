2ab75f9a02e88c838ff7d40f932b1cdd5b01a94a
Hugo <hugo.rock20@hotmail.com>
2020-06-19 00:48:57 -0500

    [ADD] swagger

--- recursos/backend-section/src/routes/idea.routes.js
+++ recursos/backend-section/src/routes/idea.routes.js
@@ -1,17 +1,25 @@
-const { Router } = require('express')
-const { ParseIntMiddleware } = require('../middleware')
+const { Router } = require("express");
+const { ParseIntMiddleware, GKPREV6 } = require("../middleware");
 
-module.exports = function({ IdeaController }){
-    const router = Router()
+module.exports = function ({ IdeaController }) {
+  const router = Router();
 
-    router.get('/', ParseIntMiddleware, IdeaController.getAll)
-    router.get('/:ideaId', IdeaController.get)
-    router.post('/', IdeaController.create)
-    router.get('/:ideaId/all', IdeaController.getUserIdeas)
-    router.patch('/:ideaId', IdeaController.update)
-    router.delete('/:ideaId', IdeaController.delete)
-    router.post('/:ideaId/upvote', IdeaController.upvoteIdeas)
-    router.post('/:ideaId/downvote', IdeaController.downvoteIdeas)
+  router.get("/", ParseIntMiddleware, IdeaController.getAll);
+  router.get("/:ideaId", ParseIntMiddleware, IdeaController.get);
+  router.post("/", ParseIntMiddleware, IdeaController.create);
+  router.get("/:ideaId/all", ParseIntMiddleware, IdeaController.getUserIdeas);
+  router.patch("/:ideaId", ParseIntMiddleware, IdeaController.update);
+  router.delete("/:ideaId", ParseIntMiddleware, IdeaController.delete);
+  router.post(
+    "/:ideaId/upvote",
+    ParseIntMiddleware,
+    IdeaController.upvoteIdeas
+  );
+  router.post(
+    "/:ideaId/downvote",
+    ParseIntMiddleware,
+    IdeaController.downvoteIdeas
+  );
 
-    return router
-}
+  return router;
+};
--- recursos/backend-section/src/routes/index.js
+++ recursos/backend-section/src/routes/index.js
@@ -1,28 +1,34 @@
-require('express-async-errors')
-const express = require('express')
-const cors = require('cors')
-const helmet = require('helmet')
-const compression = require('compression')
-const { NotFoundModdleware, ErrorMiddleware } = require('../middleware')
+require("express-async-errors");
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const compression = require("compression");
+const { NotFoundModdleware, ErrorMiddleware } = require("../middleware");
+const swaggerUI = require("swagger-ui-express");
+const { SWAGGER_DOC } = require("../config");
+const swaggerDocument = require(SWAGGER_DOC);
 
-module.exports = function({ HomeRoutes, UserRoutes, IdeaRoutes, CommentRoutes, AuthRoutes }) {
-    const router = express.Router()
-    const apiRoutes = express.Router()
-    apiRoutes
-        .use(express.json())
-        .use(cors())
-        .use(helmet())
-        .use(compression())
-    
-    apiRoutes.use('/home', HomeRoutes)
-    apiRoutes.use('/user', UserRoutes)
-    apiRoutes.use('/idea', IdeaRoutes)
-    apiRoutes.use('/comment', CommentRoutes)
-    apiRoutes.use('/auth', AuthRoutes)
+module.exports = function ({
+  HomeRoutes,
+  UserRoutes,
+  IdeaRoutes,
+  CommentRoutes,
+  AuthRoutes,
+}) {
+  const router = express.Router();
+  const apiRoutes = express.Router();
+  apiRoutes.use(express.json()).use(cors()).use(helmet()).use(compression());
 
-    router.use('/v1/api', apiRoutes)
+  apiRoutes.use("/home", HomeRoutes);
+  apiRoutes.use("/user", UserRoutes);
+  apiRoutes.use("/idea", IdeaRoutes);
+  apiRoutes.use("/comment", CommentRoutes);
+  apiRoutes.use("/auth", AuthRoutes);
 
-    router.use(NotFoundModdleware).use(ErrorMiddleware)
+  router.use("/v1/api", apiRoutes);
+  router.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
-    return router
-}
+  router.use(NotFoundModdleware).use(ErrorMiddleware);
+
+  return router;
+};
